b865aa80e2d092edd0ce3e771709f48602d5f49d
DarrelA <darrelaiscoding@gmail.com>
2022-03-03 20:17:40 +0800

    Setup socket.io connection.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+// Websockets allow for full-duplex communication.
+// Websockets is a separate protocol from HTTP.
+// Persistent connection between client and server.
+
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+io.on('connection', () => {
+  console.log('New web socket connection.');
+});
+
+server.listen(port, () => {
+  console.log(`Chat app listening on port ${port}`);
 });
