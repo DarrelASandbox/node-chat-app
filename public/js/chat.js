6c32c6610644ea962ddce5702383c5687bb93a37
DarrelA <darrelaiscoding@gmail.com>
2022-03-04 09:30:34 +0800

    Added function to send user location in chat room.

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,16 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
   socket.emit('toServerMessage', message);
   e.target.elements.message.value = '';
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation)
+    return alert('Geolocation is not supported by your browser.');
+  navigator.geolocation.getCurrentPosition((pos) => {
+    const date = new Date(pos.timestamp); // (epochTimestamp)
+
+    socket.emit(
+      'toServerMessage',
+      `${date}: https://www.google.com/maps/@${pos.coords.latitude},${pos.coords.longitude}`
+    );
+  });
+});
